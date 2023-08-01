f68c1ed1e0c1ff41e2dfd276a4c15b6ee8a96561
Smitty510 <smithpatrick.510@gmail.com>
2023-08-01 15:29:09 -0400

    gg

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -29,13 +29,15 @@ namespace IterationStatements
                 // Hint: reference num inside of the Add method's parentheses
 
 
-            } while (num < 100); // <---- While your variable is less than 100
-            {
-                while (num < 200) ; // Create a while loop  <--- While num is less than 200
+            }
+            while (num < 100); // <---- While your variable is less than 100
+
+            while (num < 200)
+            {                         // Create a while loop  <--- While num is less than 200
                 num++;             // Increment num by 1
                 numbers.Add(num); // Then add num to the collection numbers
             }
-            Console.WriteLine("Increase:");
+
 
 
 
@@ -58,7 +60,7 @@ namespace IterationStatements
             //In the scope of the foreach loop, print each number in numbers
             foreach (var n in numbers)
             {
-                Console.WriteLine($"n");
+                Console.WriteLine(n);
             }
 
             Console.WriteLine("");
@@ -71,10 +73,10 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
             // AND as long as i is greater than or equal to 0
             // Decrement i by 1
-            for (int i = 199; i <= numbers.Count && i>= 0; i--)
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
 
             {
-                Console.WriteLine($"{numbers[199]}"); // place numbers[i] inside of the Console.WriteLine() method
+                Console.WriteLine($"{numbers[i]}"); // place numbers[i] inside of the Console.WriteLine() method
 
             }
 
